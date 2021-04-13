d1f2239cef4715d4a9829f2b713a363daa1f3daf
Joseph M <jmedina1985@hotmail.com>
2021-04-12 21:27:45 -0700

    Fetch/display list

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ function App() {
         <h1>{loading ? 'loading...' : 'pagination'}</h1>
         <div className="underline"></div>
       </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+      </section>
     </main>
   );
 }
