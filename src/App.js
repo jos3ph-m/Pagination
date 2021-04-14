db10280c57aceffafef2ba7f286e5539970dc7a6
Joseph M <jmedina1985@hotmail.com>
2021-04-13 19:56:27 -0700

    Getting a single page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useFetch } from './useFetch';
 import Follower from './Follower';
 function App() {
   const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
   return (
     <main>
       <div className="section-title">
