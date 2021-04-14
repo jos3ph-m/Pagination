120248cdb68a6abedf206d504a77d62c71c3058c
Joseph M <jmedina1985@hotmail.com>
2021-04-13 19:48:53 -0700

    Paginate logic

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,10 @@ const paginate = (followers) => {
   const itemsPerPage = 9;
   //round up
   const pages = Math.ceil(followers.length / itemsPerPage);
+
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+  });
 };
 
 export default paginate;
