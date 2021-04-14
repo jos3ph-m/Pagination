17e88f78d8132d8ccfcbb029c93a7f94347a6970
Joseph M <jmedina1985@hotmail.com>
2021-04-13 19:41:24 -0700

    Paginate logic

--- src/useFetch.js
+++ src/useFetch.js
@@ -11,6 +11,7 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url);
     const data = await response.json();
+    paginate(data);
     setData(data);
     setLoading(false);
   };
